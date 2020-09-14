9274161a6f93079747b8a321df1c6cc29617deb7
Matheo <math.dumont.md@gmail.com>
2020-09-14 10:54:30 +0200

    update cmake + kernel

---
+++ include/kernel.h
@@ -0,0 +1,21 @@
+#ifndef KERNEL_H
+#define KERNEL_H
+
+class Kernel
+{
+private:
+    /* data */
+public:
+    Kernel(/* args */);
+    ~Kernel();
+};
+
+Kernel::Kernel(/* args */)
+{
+}
+
+Kernel::~Kernel()
+{
+}
+
+#endif
