83d660cb86b1b8c71b452f4fab88794570634949
Matheo <math.dumont.md@gmail.com>
2020-09-14 10:29:45 +0200

    exemple main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,26 @@
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/video/background_segm.hpp>
+
+/*
+* Partie 1 Tp 1 Appliquer convolution sur Image
+*/
+
 int main()
 {
-    return 0;
+    cv::Mat image = cv::imread(path);
+
+    if (!image.data)
+    {
+        std::cerr << "Error : image or video not found" << std::endl;
+        exit(-1);
+    }
+
+    cv::imshow("Image_from_path", image);
+
+    cv::waitKey(0);
+    cv::destroyAllWindows();
 }
