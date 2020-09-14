6817847bb9e5018bb132b4a11ca64f56511cd241
ygazull <yann-situ.gazull@ens-lyon.fr>
2020-09-14 12:46:12 +0200

    [UPD] add kernel files

---
+++ src/kernel.cpp
@@ -0,0 +1,10 @@
+#include "kernel.h"
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/video/background_segm.hpp>
+
+#include <iostream>
